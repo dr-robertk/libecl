eb236a3935ffa2b271b68e7e3725195645d966db
Robert K <robertk@posteo.org>
2017-12-06 22:35:17 +0100

    small changes.

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ INSTALLDIR=`pwd`/../libecl-install
 rm -rf build
 mkdir build
 cd build
-cmake ../ -DCMAKE_INSTALL_PREFIX=$INSTALLDIR -DBOOST_ROOT=/home/robertk/work/Dune/modules/boost/1.59_gcc4.9.4/ -DBUILD_APPLICATIONS=ON
+cmake ../ -DCMAKE_INSTALL_PREFIX=$INSTALLDIR -DBUILD_APPLICATIONS=ON
 #  -DPING_PATH=/bin
 make -j4
 make install
