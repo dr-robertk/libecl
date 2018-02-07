cea549b0ec5fefec8aedf4df1e6e635aa7d2ab3e
Robert Kloefkorn <robert.kloefkorn@iris.no>
2018-02-07 13:22:02 +0100

    simple build script.

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+INSTALLDIR=`pwd`/../libecl-install
+
+rm -rf build
+mkdir build
+cd build
+cmake ../ -DCMAKE_INSTALL_PREFIX=$INSTALLDIR -DBUILD_APPLICATIONS=ON
+#  -DPING_PATH=/bin
+make -j4
+make install
+cd ../
