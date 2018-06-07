930af980f7c8845cb88cbd5faf77b58f1920245a
Robert Kloefkorn <robert.kloefkorn@iris.no>
2018-06-07 14:13:26 +0200

    Only build static libs.

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ INSTALLDIR=`pwd`/install
 rm -rf build
 mkdir build
 cd build
-cmake ../ -DCMAKE_INSTALL_PREFIX=$INSTALLDIR -DBUILD_APPLICATIONS=ON
+cmake ../ -DCMAKE_INSTALL_PREFIX=$INSTALLDIR -DBUILD_APPLICATIONS=ON -DBUILD_SHARED_LIBS=OFF
 #  -DPING_PATH=/bin
 make -j4
 make install
